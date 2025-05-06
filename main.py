8cc04fcd9765ef533f1c6675749e1923a35f2262
feikai <974086665@qq.com>
2025-05-06 09:34:26 +0800

    feat(train visualization):add wandb visualization to see the real time training process.

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from optuna.visualization import plot_optimization_history, plot_param_importanc
 from gymnasium.envs.registration import register
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import SAC, TD3
-from stable_baselines3.common.callbacks import (
-    ProgressBarCallback,
-    BaseCallback,
-    CallbackList,
-)
+from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.evaluation import evaluate_policy
+
+import wandb
+from wandb.integration.sb3 import WandbCallback
+
 
 import numpy as np
 import torch
@@ -20,6 +20,7 @@ from gymnasium.wrappers import TimeLimit
 import warnings
 
 warnings.filterwarnings("ignore")
+
 
 # 固定随机种子
 SEED = 42
@@ -44,6 +45,7 @@ class EpisodeMetricCallback(BaseCallback):
         """
 
         super().__init__(verbose)
+        self.episode_counter = 0  # 记录episode数量
         self.metrics_to_track = metrics_to_track
         self.episode_metrics: Dict[str, List[float]] = {}  # 存储所有episode的均值
         self.current_episode_metrics: Dict[str, List[float]] = {}  # 当前episode的原始值
@@ -54,6 +56,7 @@ class EpisodeMetricCallback(BaseCallback):
         done = self.locals["dones"][0]
 
         # 首次运行时初始化字典
+        # {} 是一个已经实例化的对象（有地址），只是内容为空。这种情况下if not还是会判断为true
         if not self.current_episode_metrics:
             self._init_metrics(info)
 
@@ -61,12 +64,18 @@ class EpisodeMetricCallback(BaseCallback):
             self.current_episode_metrics[metric].append(info[metric])
 
         if done:
+            self.episode_counter += 1  # 增加episode数量
+            log_dict = {}  # 记录到wandb的字典
             for metric, values in self.current_episode_metrics.items():
                 mean_value = np.mean(values) if values else 0.0
                 self.episode_metrics[metric].append(mean_value)
-                self.logger.record(f"episode/{metric}", mean_value, exclude="stdout")
+                # self.logger.record(
+                #     f"episode/{metric}", mean_value, self.episode_counter
+                # )
+                log_dict[f"episode/{metric}"] = mean_value  # 添加到 wandb 日志
                 values.clear()  # 清空当前episode的值
                 # self.current_episode_metrics[metric].clear()
+            wandb.log(log_dict, step=self.episode_counter)  # 同步到 wandb
 
         return True
 
@@ -160,14 +169,17 @@ def main():
     env1 = gym.make("UAVEnv-v0")
     env1.reset(seed=SEED)  # 设置随机种子以确保可重复性
 
-    metric_callback = EpisodeMetricCallback(verbose=1)
-    progress_bar_callback = ProgressBarCallback()
-    combined_callback = CallbackList(
-        [
-            metric_callback,
-            progress_bar_callback,  # 显示进度条
-        ]
+    wandb.init(
+        project="UAV-SAC_1",  # 项目名称（wandb 仪表盘中显示）
+        name="experiment-2",  # 实验名称（可选）
+        config={  # 记录超参数（可选）
+            "policy": "MlpPolicy",
+            "total_timesteps": 10000,
+        },
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
     )
+
+    metric_callback = EpisodeMetricCallback(verbose=1)
 
     # env1._get_obs()
     check_env(env1.unwrapped, skip_render_check=True)
@@ -186,10 +198,11 @@ def main():
 
     # 训练模型（带进度条）
     model.learn(
-        total_timesteps=100000,
-        callback=combined_callback,  # 显示进度条
+        total_timesteps=10000,
+        callback=metric_callback,  # 显示进度条
         log_interval=10,  # 每10步打印一次日志
     )
+    wandb.finish()
 
     # 保存模型
 
@@ -239,7 +252,7 @@ def TD3_test():
 
 def test_model():
     # 加载模型
-    model = SAC.load("sac_uav_model")
+    model = SAC.load("sac_uav_model_1746280931")
     # 创建环境
     # 创建环境
     env = gym.make("UAVEnv-v0", render_mode="human")
@@ -280,7 +293,7 @@ def test_model():
 
 
 if __name__ == "__main__":
-    # main()
-    test_model()
+    main()
+    # test_model()
     # TD3_test()
     # find_best_hyperparameters()
